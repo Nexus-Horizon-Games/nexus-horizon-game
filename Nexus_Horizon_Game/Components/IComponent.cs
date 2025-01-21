efba85bf61c2196985511847de990f8048a39b98
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-01-21 13:05:16 -0800

    Added basic architecture

---
+++ Nexus_Horizon_Game/Components/IComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus_Horizon_Game.Components
+{
+    internal interface IComponent
+    {
+    }
+}
---
+++ Nexus_Horizon_Game/Scene.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Nexus_Horizon_Game
+{
+    internal class Scene
+    {
+        private EntityComponentSystem ecs = new EntityComponentSystem();
+
+        public void Update(GameTime gameTime)
+        {
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+        }
+
+        public EntityComponentSystem ECS { get { return ecs; } }
+    }
+}
