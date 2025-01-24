460b79e262790807ace59af9de57b26c39fb91dd
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-01-24 09:36:38 -0800

    Better SpriteComponent and fixed IsEntityAlive bug

--- Nexus_Horizon_Game/Components/SpriteComponent.cs
+++ Nexus_Horizon_Game/Components/SpriteComponent.cs
@@ -1,26 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Nexus_Horizon_Game.Components
 {
     internal struct SpriteComponent : IComponent
     {
+        public int spriteID = -1;
+        public Vector2 position = Vector2.Zero;
+        public float rotation = 0.0f;
+        public Color color = Color.White;
+        public float scale = 1.0f;
+        public float z = 0.0f; // between 0 and 1
+        public Rectangle? sourceRectangle = null; // used to render only a section of an image (for tiles)
+
         public SpriteComponent(int spriteID)
         {
             this.spriteID = spriteID;
         }
-
-        int spriteID = -1;
 
         /// <inheritdoc/>
         public bool Equals(IComponent other)
         {
             if (other is SpriteComponent o)
             {
-                if (spriteID == o.spriteID)
+                if (spriteID == o.spriteID &&
+                    position == o.position &&
+                    rotation == o.rotation &&
+                    color == o.color &&
+                    scale == o.scale &&
+                    z == o.z &&
+                    sourceRectangle == o.sourceRectangle)
                 {
                     return true;
                 }
