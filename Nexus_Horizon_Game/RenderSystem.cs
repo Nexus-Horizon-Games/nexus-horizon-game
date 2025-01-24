57440784ccc52939d8c1346793f66c530e9ebb76
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-01-23 22:32:35 -0800

    Started adding Renderer class

--- Nexus_Horizon_Game/RenderSystem.cs
+++ Nexus_Horizon_Game/RenderSystem.cs
@@ -8,7 +8,7 @@ namespace Nexus_Horizon_Game
 {
     internal class RenderSystem
     {
-        public static void Draw(World world, GameTime gameTime, SpriteBatch spriteBatch, Texture2D spriteTexture) // spriteBatch and spriteTexture being passed here should only be temporary
+        public static void Draw(World world, GameTime gameTime, Texture2D spriteTexture) // spriteTexture being passed here should only be temporary
         {
             var transformComponents = world.GetComponents<TransformComponent>().ToList();
             var spriteComponents = world.GetComponents<SpriteComponent>().ToList();
@@ -20,7 +20,8 @@ namespace Nexus_Horizon_Game
 
                 if (!transformComp.IsEmptyComponent() && !spriteComp.IsEmptyComponent())
                 {
-                    spriteBatch.Draw(spriteTexture, transformComp.position, Color.White);
+                    Renderer.Draw(spriteTexture, transformComp.position, Color.White);
+                    Renderer.Draw(spriteTexture, transformComp.position, null, Color.White, (float)transformComp.rotation, Vector2.Zero, 5.0f, SpriteEffects.None, 0.0f);
                 }
             }
         }
--- Nexus_Horizon_Game/Scene.cs
+++ Nexus_Horizon_Game/Scene.cs
@@ -13,10 +13,10 @@ namespace Nexus_Horizon_Game
             // get component enumeration then through each component do the update for it.
         }
 
-        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Texture2D spriteTexture) // spriteBatch and spriteTexture being passed here should only be temporary
+        public void Draw(GameTime gameTime, Texture2D spriteTexture) // spriteTexture being passed here should only be temporary
         {
 
-            RenderSystem.Draw(world, gameTime, spriteBatch, spriteTexture);
+            RenderSystem.Draw(world, gameTime, spriteTexture);
             // Get entities with component
             // get component enumeration then through each component do the draw for it.
         }
