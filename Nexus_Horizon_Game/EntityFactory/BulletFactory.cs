2948be84caedddac8457b72aadb0cfb7c03f9a9c
ChanGu3 <154095535+ChanGu3@users.noreply.github.com>
2025-01-31 12:25:06 -0800

    feat: bullet projectile

--- Nexus_Horizon_Game/EntityFactory/BulletFactory.cs
+++ Nexus_Horizon_Game/EntityFactory/BulletFactory.cs
@@ -6,11 +6,11 @@ namespace Nexus_Horizon_Game.EntityFactory
 {
     internal class BulletFactory : EntityFactory
     {
-        private Scene scene;
+        private World world;
         private string textureName;
-        public BulletFactory(ref Scene scene, string textureName)
+        public BulletFactory(World world, string textureName)
         {
-            this.scene = scene;
+            this.world = world;
             this.textureName = textureName;
         }
 
@@ -20,10 +20,31 @@ namespace Nexus_Horizon_Game.EntityFactory
         /// <returns> entity ID. </returns>
         public override int CreateEntity()
         {
-            return scene.World.CreateEntity(new List<IComponent>
+            return world.CreateEntity(new List<IComponent>
             { new TransformComponent(new Vector2(0.0f, 0.0f)),
-                new SpriteComponent(textureName, color: Color.White, scale: 0.25f, z: 0.0f),
+                new SpriteComponent(textureName, color: Color.White, scale: 0.25f, spriteLayer: 0),
                 new PhysicsBody2DComponent(),
+              new BulletComponent() });
+        }
+
+        /// <summary>
+        /// creates a moving bullet at a specific point and direction
+        /// </summary>
+        /// <param name="SpawnPoint"> where the bullet will spawn. </param>
+        /// <param name="direction"> the direction the bullet will move (Will be normalized). </param>
+        /// <param name="velocity"> the speed the bullet will move in that direction. </param>
+        /// <returns> entity id. </returns>
+        public int CreateEntity(Vector2 SpawnPoint, Vector2 direction, float velocity)
+        {
+            direction.Normalize();
+
+            return world.CreateEntity(new List<IComponent>
+            { new TransformComponent(SpawnPoint),
+                new SpriteComponent(textureName, color: Color.White, scale: 0.25f, spriteLayer: 0),
+                new PhysicsBody2DComponent()
+                {
+                    Velocity = new Vector2(velocity * direction.X, velocity * direction.Y),
+                },
               new BulletComponent() });
         }
     }
