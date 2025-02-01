29baae37292ac9b6d77df1c9203cca1e2c86a729
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-01-31 21:04:47 -0800

    Added a simple path class for enemy movement

---
+++ Nexus_Horizon_Game/Paths/Path.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Nexus_Horizon_Game.Paths
+{
+    internal interface IPath
+    {
+        /// <summary>
+        /// Gets a point on the path.
+        /// </summary>
+        /// <param name="t">Should be between 0 and 1.</param>
+        /// <returns>The point.</returns>
+        Vector2 GetPoint(float t);
+
+        /// <summary>
+        /// Gets the derivative of a point on the curve.
+        /// </summary>
+        /// <param name="t">Should be between 0 and 1.</param>
+        /// <returns>The derivative.</returns>
+        Vector2 GetDerivative(float t);
+
+        /// <summary>
+        /// Gets the change in 't', given the speed.
+        /// </summary>
+        /// <param name="t">Should be between 0 and 1.</param>
+        /// <param name="speed">The speed.</param>
+        /// <returns>The change in 't' that chould take place.</returns>
+        float GetDeltaT(float t, float speed);
+    }
+}
---
+++ Nexus_Horizon_Game/Paths/QuadraticCurvePath.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Nexus_Horizon_Game.Paths
+{
+    internal struct QuadraticCurvePath : IPath
+    {
+        public Vector2 start;
+        public Vector2 control;
+        public Vector2 end;
+
+        // chached vectors to calculate the derivative
+        private readonly Vector2 v1;
+        private readonly Vector2 v2;
+
+        public QuadraticCurvePath(Vector2 start, Vector2 control, Vector2 end)
+        {
+            this.start = start;
+            this.control = control;
+            this.end = end;
+
+            this.v1 = 2 * start - 4 * control + 2 * end;
+            this.v2 = -2 * start + 2 * control;
+        }
+
+        /// <inheritdoc/>
+        public Vector2 GetPoint(float t)
+        {
+            return ((1.0f - t) * (1 - t)) * start +
+                (2.0f * (1.0f - t) * t) * control +
+                (t * t) * end;
+        }
+
+        /// <inheritdoc/>
+        public Vector2 GetDerivative(float t)
+        {
+            return t * v1 + v2;
+        }
+
+        /// <inheritdoc/>
+        public float GetDeltaT(float t, float speed)
+        {
+            // Help from: https://gamedev.stackexchange.com/questions/27056/how-to-achieve-uniform-speed-of-movement-on-a-bezier-curve
+            return speed / GetDerivative(t).Length();
+        }
+    }
+}
