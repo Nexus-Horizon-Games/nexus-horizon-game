b73b9d261fb1f076ed28339778c9cdcf3ac27242
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-01-31 22:23:31 -0800

    Added timer component

--- Nexus_Horizon_Game/Entity Type Behaviours/ChefBossBehaviour.cs
+++ Nexus_Horizon_Game/Entity Type Behaviours/ChefBossBehaviour.cs
@@ -1,6 +1,9 @@
 ﻿
 using Microsoft.Xna.Framework;
 using Nexus_Horizon_Game.Components;
+using Nexus_Horizon_Game.EntityFactory;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Nexus_Horizon_Game.Entity_Type_Behaviours
 {
@@ -21,21 +24,21 @@ namespace Nexus_Horizon_Game.Entity_Type_Behaviours
             Stage2          // the second stage of the fight
         }
 
-        public static void OnUpdate(World world, int thisEntity, GameTime gameTime)
+        public static void OnUpdate(Scene scene, int thisEntity, GameTime gameTime)
         {
-            var state = world.GetComponentFromEntity<StateComponent>(thisEntity);
+            var state = scene.World.GetComponentFromEntity<StateComponent>(thisEntity);
 
             if ((ChefBossState)state.state == ChefBossState.Start)
             {
-                StartState(world, thisEntity);
+                StartState(scene.World, scene, thisEntity);
             }
             else if ((ChefBossState)state.state == ChefBossState.EnteringArena)
             {
-                EnteringArenaState(world, thisEntity);
+                EnteringArenaState(scene.World, thisEntity);
             }
             else if ((ChefBossState)state.state == ChefBossState.Stage1)
             {
-
+                
             }
             else if ((ChefBossState)state.state == ChefBossState.Stage2)
             {
@@ -43,8 +46,12 @@ namespace Nexus_Horizon_Game.Entity_Type_Behaviours
             }
         }
 
-        private static void StartState(World world, int thisEntity)
+        private static void StartState(World world, Scene scene, int thisEntity)
         {
+            var timerComp = new TimersComponent([]);
+            timerComp.timers.Add("fire_bullets", new Timer(0.2f, OnFireBullets, (scene, thisEntity)));
+            world.AddComponent(thisEntity, timerComp);
+
             world.SetComponentInEntity(thisEntity, new TransformComponent(new Vector2(Renderer.DrawAreaWidth / 2.0f, -20.0f)));
 
             // Start moving into the arena
@@ -66,7 +73,32 @@ namespace Nexus_Horizon_Game.Entity_Type_Behaviours
                 world.SetComponentInEntity(thisEntity, body);
 
                 world.SetComponentInEntity(thisEntity, new StateComponent(ChefBossState.Stage1));
+                var timers = world.GetComponentFromEntity<TimersComponent>(thisEntity);
+                timers.timers["fire_bullets"].Start();
             }
+        }
+
+        private static void OnFireBullets(GameTime gameTime, object? data)
+        {
+            var (scene, thisEntity) = ((Scene, int))data;
+
+            var bulletFactory = new BulletFactory(ref scene, "BulletSample");
+            var bullet = bulletFactory.CreateEntity();
+
+            var bossPosition = scene.World.GetComponentFromEntity<TransformComponent>(thisEntity).position;
+
+            var players = scene.World.GetEntitiesWithComponent<PlayerComponent>().ToList();
+            var playerPosition = scene.World.GetComponentFromEntity<TransformComponent>(players[0]).position;
+
+            var transform = scene.World.GetComponentFromEntity<TransformComponent>(bullet);
+            transform.position = bossPosition;
+            scene.World.SetComponentInEntity(bullet, transform);
+
+            var body = scene.World.GetComponentFromEntity<PhysicsBody2DComponent>(bullet);
+            var direction = playerPosition - transform.position;
+            direction.Normalize();
+            body.Velocity = direction * 60.0f;
+            scene.World.SetComponentInEntity(bullet, body);
         }
     }
 }
