acd461d2e7cd991cba3b407831381f918245f0cf
JosephBuchholz <70069846+JosephBuchholz@users.noreply.github.com>
2025-02-05 15:00:52 -0800

    Chef Boss: better movement

--- Nexus_Horizon_Game/SceneLoader.cs
+++ Nexus_Horizon_Game/SceneLoader.cs
@@ -1,5 +1,3 @@
-using Microsoft.Xna.Framework;
-using Nexus_Horizon_Game.Components;
 
 using Nexus_Horizon_Game.EntityFactory;
 
@@ -12,17 +10,8 @@ namespace Nexus_Horizon_Game
             currentScene = new Scene();
             // TODO: parse the scene from JSON here
 
-            // For Testing
-            int npc1 = currentScene.World.CreateEntity();
-            var sprite = new SpriteComponent("guinea_pig", spriteLayer: 100);
-            sprite.scale = 1.0f;
-            currentScene.World.AddComponent(npc1, new TransformComponent(new Vector2(5.0f, 10.0f)));
-            currentScene.World.AddComponent(npc1, sprite);
-
             var playerFactory = new PlayerFactory();
             int moveablePlayer2 = playerFactory.CreateEntity();
-
-            var bulletFactory = new BulletFactory("BulletSample");
 
             EnemyFactory.CreateBoss("chef_boss");
         }
