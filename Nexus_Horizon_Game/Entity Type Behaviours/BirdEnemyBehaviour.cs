7de8f3edda785a1d078c2ae296df28709d82a222
dbrighthd <159100932+dhofsetz@users.noreply.github.com>
2025-02-09 19:56:23 -0800

    Changed the way bird enemies are loaded
    Now it works with a multipath + a list of paths to fire on.
    added sample path creators in enemy factory, these will later be loaded from json

--- Nexus_Horizon_Game/Entity Type Behaviours/BirdEnemyBehaviour.cs
+++ Nexus_Horizon_Game/Entity Type Behaviours/BirdEnemyBehaviour.cs
@@ -5,37 +5,38 @@ using Nexus_Horizon_Game.Timers;
 using Nexus_Horizon_Game.Paths;
 using System.Collections.Generic;
 using System.Collections;
+using System.Linq;
 
 namespace Nexus_Horizon_Game.Entity_Type_Behaviours
 {
     internal class BirdEnemyBehaviour : Behaviour
     {
-        private float Speed = 8.0f;
-        private float AttackSpeed = 5.0f;
-        private float LeaveSpeed = 10.0f;
         private float FireRate = 0.2f;
         private float t;
         private float waitTime = 0;
+        private float speed = 0.4f;
         private BulletFactory bulletFactory = new BulletFactory("BulletSample");
         private TimerContainer timerContainer = new TimerContainer();
         private Vector2[] attackPoints;
-        private Dictionary<BirdEnemyState, IPath> pathPattern = new Dictionary<BirdEnemyState, IPath>();
+        private MultiPath movementPath;
+        private int[] attackPaths;
 
         public enum BirdEnemyState : int
         {
             None,
             Start,
-            EnteringArena,
-            Attacking,
-            LeavingArena
+            Moving,
+            End,
         }
 
-        public BirdEnemyBehaviour(int thisEntity, Vector2[] attackPoints, float waitTime) : base(thisEntity) 
+        public BirdEnemyBehaviour(int thisEntity, MultiPath movementPath, int[] attackPaths, float waitTime) : base(thisEntity)
         {
-            this.attackPoints = attackPoints;
+            this.movementPath = movementPath;
+            this.attackPaths = attackPaths;
             this.waitTime = waitTime;
         }
 
+        
         public override void OnUpdate(GameTime gameTime)
         {
             var state = GameM.CurrentScene.World.GetComponentFromEntity<StateComponent>(this.Entity);
@@ -46,14 +47,11 @@ namespace Nexus_Horizon_Game.Entity_Type_Behaviours
                 case BirdEnemyState.Start:
                     StartState();
                     break;
-                case BirdEnemyState.EnteringArena:
-                    EnteringArenaState(gameTime);
+                case BirdEnemyState.Moving:
+                    MovingState(gameTime);
                     break;
-                case BirdEnemyState.Attacking:
-                    AttackingState(gameTime);
-                    break;
-                case BirdEnemyState.LeavingArena:
-                    LeavingState(gameTime);
+                case BirdEnemyState.End:
+                    EndState(gameTime);
                     break;
             }
         }
@@ -67,59 +65,39 @@ namespace Nexus_Horizon_Game.Entity_Type_Behaviours
                 return;
             }
             t = 0;
-            setAttackPoints();
-            GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new StateComponent(BirdEnemyState.EnteringArena));
+            timerContainer.AddTimer(new LoopTimer(FireRate, (gameTime, data) => OnFireBullets(gameTime)), "fire");
+            GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new StateComponent(BirdEnemyState.Moving));
         }
-
-        private void setAttackPoints()
-        {
-            pathPattern[BirdEnemyState.EnteringArena] = new QuadraticCurvePath(attackPoints[0], new Vector2(attackPoints[0].X, attackPoints[1].Y), attackPoints[1]);
-            pathPattern[BirdEnemyState.Attacking] = new QuadraticCurvePath(attackPoints[1], new Vector2((attackPoints[1].X + attackPoints[2].X) / 2, ((attackPoints[1].Y + attackPoints[2].Y) / 2)), attackPoints[2]);
-            pathPattern[BirdEnemyState.LeavingArena] = new QuadraticCurvePath(attackPoints[2], new Vector2(attackPoints[3].X, attackPoints[2].Y), attackPoints[3]);
-        }
-        private void EnteringArenaState(GameTime gameTime)
+        private void MovingState(GameTime gameTime)
         {
             if (t >= 1) 
             {
-                timerContainer.AddTimer(new LoopTimer(FireRate, (gameTime, data) => OnFireBullets(gameTime)), "fire");
-                timerContainer.GetTimer("fire").Start();
-                t = 0;
-                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new StateComponent(BirdEnemyState.Attacking));
+                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new StateComponent(BirdEnemyState.End));
+                return;
+            }
+            if (attackPaths.Contains(movementPath.getIndex(t)))
+            {
+                if (!timerContainer.GetTimer("fire").IsOn)
+                {
+                    timerContainer.GetTimer("fire").Start();
+                }
             }
             else
             {
-                t += 0.01f;
-                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new TransformComponent(pathPattern[BirdEnemyState.EnteringArena].GetPoint(t)));
+                if (timerContainer.GetTimer("fire").IsOn)
+                {
+                    timerContainer.GetTimer("fire").Stop();
+                }
             }
-        }
-
-        private void AttackingState(GameTime gameTime)
-        {
-            if (t >= 1)
-            {
-                timerContainer.GetTimer("fire").Stop();
-                t = 0;
-                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new StateComponent(BirdEnemyState.LeavingArena));
-            }
-            else
-            {
-                t += 0.01f;
-                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new TransformComponent(pathPattern[BirdEnemyState.Attacking].GetPoint(t)));
-            }
+            t += movementPath.GetDeltaT(t,speed);
+            GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new TransformComponent(movementPath.GetPoint(t)));
         }
 
 
-        private void LeavingState(GameTime gameTime)
+
+        private void EndState(GameTime gameTime)
         {
-            if (t >= 1)
-            {
-                GameM.CurrentScene.World.DestroyEntity(this.Entity);
-            }
-            else
-            {
-                t += 0.01f;
-                GameM.CurrentScene.World.SetComponentInEntity(this.Entity, new TransformComponent(pathPattern[BirdEnemyState.LeavingArena].GetPoint(t)));
-            }
+               GameM.CurrentScene.World.DestroyEntity(this.Entity);
         }
 
         private void OnFireBullets(GameTime gameTime)
